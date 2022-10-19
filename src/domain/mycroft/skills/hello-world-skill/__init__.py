f46934adfb9b24e20dcf7c71a7400aa1194d9b11
Miguel Teixeira <miguel.teixeira@poli.ufrj.br>
2022-10-18 21:58:07 -0300

    WIP mycroft scripts

--- src/domain/mycroft/skills/hello-world-skill/__init__.py
+++ src/domain/mycroft/skills/hello-world-skill/__init__.py
@@ -1,14 +1,25 @@
 from mycroft import MycroftSkill, intent_handler
+import requests
 
 
 class HelloWorld(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
+    #
+
     @intent_handler("world.hello.intent")
     def handle_world_hello(self, message):
         self.speak_dialog("world.hello")
 
+    #
+
+
+#
+
 
 def create_skill():
     return HelloWorld()
+
+
+#
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 import { initialize } from './app'
+import { HelloCommand } from './domain/vscode/commands/HelloCommand'
 
 initialize()
 
@@ -10,22 +11,6 @@ initialize()
 export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log(
-        'Congratulations, your extension "helloworld-sample" is now active!'
-    )
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand(
-        'extension.helloWorld',
-        () => {
-            // The code you place here will be executed every time your command is executed
-
-            // Display a message box to the user
-            vscode.window.showInformationMessage('Hello World!')
-        }
-    )
-
-    context.subscriptions.push(disposable)
+    const hello = new HelloCommand()
+    hello.register(context)
 }
